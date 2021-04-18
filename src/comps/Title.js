7eb1a41716d179fd719862ad74ff0e9c43e2b096
Nikolaos <nzoump@gmail.com>
2021-04-18 14:12:03 +0200

    Init commit

---
+++ src/comps/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireGram</h1>
+      <h2>Your Pictures</h2>
+      <h3>Web Developer Nikos Zoumpas</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+};
+
+export default Title;
